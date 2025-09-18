cc540654f2aa5d2bd8831a93242964f359d78a29
haydenfee <haydenfee@uvic.ca>
2025-09-18 13:29:11 -0700

    Pushing files to remote repository

---
+++ demo/src/main/java/com/example/ImpactCLI.java
@@ -0,0 +1,29 @@
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "request", mixinStandardHelpOptions = true, version = "request 1.0", description = "Use option -n to remove request for a job")
+public class ImpactCLI implements Callable<Integer> {
+    public static void request(String text) {
+        System.out.println(text);
+    }
+
+    @Option(names = {"-n", "--no"}, description = "Removes my request for a job")
+    private String requestString = "Yes";
+
+    public Integer call() {
+        if (requestString == "Yes") {
+            System.out.println("Please hire me!!");
+        }
+        else {
+            System.out.println(":(");
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new ImpactCLI()).execute(args);
+        System.exit(exitCode);
+    }
+}
